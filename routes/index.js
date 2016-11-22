725f7e7ef572b89c491bd439dfdd4a468ee322ca
Alexander Murphy <amurphy345@gmail.com>
2016-11-22 14:11:42 -0700

    mapper class added

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 const webScrapingController = require('../controllers/webScrapingController.js')
 const webScrapingControllerClass = new webScrapingController
+
+const lowEndBoxScrapingController = require('../controllers/lowEndBoxScrapingController')
+const lowEndBoxScrapingControllerClass = new lowEndBoxScrapingController
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -17,6 +21,10 @@ router.get('/detail', (req, res, next) => {
 router.get('/allPages', (req, res, next) => {
   // res.json(webScrapingControllerClass.getAllPageLinks())
   webScrapingControllerClass.getAllPageLinks().then(data => res.json(data))
+})
+
+router.get('/scrapeRawHTML', (req, res, next) => {
+  res.json(lowEndBoxScrapingControllerClass.parseFromFile())
 })
 
 module.exports = router;
